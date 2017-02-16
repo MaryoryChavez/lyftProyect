8107c8eb01f87bf8438ea672f66b67a90f56add6
MaryoryChavez <maryory.chavez@gmail.com>
2017-02-16 16:57:39 -0500

    skins 90%

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,33 @@
-function listCountries(evt) {
-    var listCountries = document.getElementById("listCountries");
-    var nombre = document.getElementById("valorCount");
     var input = document.getElementById("inputNumber");
-    for (var i = 0; i < countries.length; i++) {
-        var li = document.createElement("LI");
-        li.innerHTML = '<div src="" class="flag flag-' + countries[i].code.toLowerCase() + '" alt="" >' + '</div>' + '<a href="" onclick="dial(' + i + ',event)">' + countries[i].name + '</a>';
+    var dialNumber = document.getElementById("dialNumber");
 
-        input.value = "+51";
-        nombre.innerHTML = '<div src="" class="flag flag-pe" alt=""></div>' + '</div>' + '<p>Perú</p>';
+    function listCountries(evt) {
+        var listCountries = document.getElementById("listCountries");
+        var nombre = document.getElementById("valorCount");
+        for (var i = 0; i < countries.length; i++) {
+            var li = document.createElement("LI");
+            li.innerHTML = '<div src="" class="flag flag-' + countries[i].code.toLowerCase() + '" alt="" >' + '</div>' + '<a href="" onclick="dial(' + i + ',event)">' + countries[i].name + '</a>';
 
-        listCountries.appendChild(li);
+            dialNumber.innerHTML = "+51";
+            nombre.innerHTML = '<div src="" class="flag flag-pe" alt=""></div>';
+            listCountries.appendChild(li);
+        }
+        console.log(countries[0].code.toLowerCase());
+        evt.preventDefault();
     }
-    console.log(countries[0].code.toLowerCase());
-    evt.preventDefault();
-}
 
-function dial(num, evt) {
-    var input = document.getElementById("inputNumber");
-    var nombre = document.getElementById("valorCount");
-    input.value = countries[num].dial_code;
-    nombre.innerHTML = '<div src="" class="flag flag-' + countries[num].code.toLowerCase() + '" alt="" >' + '</div>' + '<p>' + countries[num].name + '</p>';
+    function dial(num, evt) {
+        var nombre = document.getElementById("valorCount");
+        dialNumber.innerHTML = countries[num].dial_code;
+        nombre.innerHTML = '<div src="" class="flag flag-' + countries[num].code.toLowerCase() + '" alt="" >' + '</div>';
+        evt.preventDefault();
+    }
 
-    evt.preventDefault();
-}
-
-function aleatoriPass() {
-    var aleatory = "LAB-" + Math.round(Math.random() * (999 - 100) + parseInt(100));
-    alert(aleatory);
-    location.href="signup2.html";
-}
+    function aleatoriPass() {
+        var aleatory = "LAB-" + Math.round(Math.random() * (999 - 100) + parseInt(100));
+        alert("Tu codigo es: " + aleatory);
+        localStorage.setItem("codigo", aleatory);
+        var cellphone = dialNumber.innerHTML + "" + input.value;
+        localStorage.setItem("cellphone", cellphone);
+        location.href = "signup2.html";
+    }
